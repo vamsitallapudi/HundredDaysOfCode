4d2d583e29b76f003d4d7c65614e639f3eafd203
Vamsi Tallapudi <mail4vamsikrishna@gmail.com>
2023-12-22 20:16:26 +0530

    100 Days of Code - Day 2 - Sum of all elements

---
+++ src/arrays/beginner/SumOfAllElements.java
@@ -0,0 +1,12 @@
+package arrays.beginner;
+
+public class SumOfAllElements {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        System.out.println(sum);
+    }
+}
